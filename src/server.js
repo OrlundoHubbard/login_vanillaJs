3f6545da20cdb12750511cbbd836e5156b7f5297
Orlundo Hubbard <orlundokhubbard@gmail.com>
2022-11-04 20:23:33 -0500

    add server

---
+++ src/server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const { join } = require("path");
+const app = express();
+
+// Serve static assets from the /public folder
+app.use(express.static(join(__dirname, "public")));
